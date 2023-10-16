ac1b85e0a21ded987696c4695982528524d40744
Victor Demetrius Marqueto <73915190+VictorDemetrius@users.noreply.github.com>
2023-10-16 19:57:33 -0300

    Add files via upload

---
+++ sigvs/src/main/java/com/fatec/sigvs/controller/APIProdutoController.java
@@ -0,0 +1,88 @@
+package com.fatec.sigvs.controller;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Optional;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fatec.sigvs.model.Imagem;
+import com.fatec.sigvs.model.Produto;
+import com.fatec.sigvs.servico.IProdutoServico;
+import com.fatec.sigvs.servico.ImagemServico;
+
+@RestController
+@RequestMapping("/api/v1/produtos")
+public class APIProdutoController {
+	Logger logger = LogManager.getLogger(this.getClass());
+	@Autowired
+	IProdutoServico produtoServico;
+	@Autowired
+	ImagemServico imagemServico;
+
+	/**
+	 * Consulta catalogo
+	 * 
+	 * @return - JSON Array com todos os produtos ou um JSON Array vazio
+	 */
+	@CrossOrigin // desabilita o cors do spring security
+	@GetMapping
+	public ResponseEntity<Object> consultaTodos() {
+		logger.info(">>>>>> apicontroller consulta todos");
+		return ResponseEntity.status(HttpStatus.OK).body(produtoServico.consultaCatalogo());
+	}
+
+	@CrossOrigin
+	@PostMapping
+	public ResponseEntity<Object> cadastraProduto(@RequestBody Produto p) {
+		logger.info(">>>>>> apicontroller cadastrar produto iniciado");
+		Optional<Produto> produto = produtoServico.cadastrar(p);
+		if (produto.isPresent()) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(produto.get());
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+	}
+
+	@CrossOrigin // desabilita o cors do spring security
+	@PostMapping("/imadb")
+	public ResponseEntity<String> upload(@RequestParam(value = "file") MultipartFile file, @RequestParam String id) {
+		logger.info(">>>>>> api upload iniciada...");
+		try {
+			logger.info(">>>>>> api manipula file upload chamou servico salvar");
+			long codProduto = Long.parseLong(id);
+			Optional<Imagem> i = imagemServico.salvar(file, codProduto);
+			if (i.isPresent()) {
+				return ResponseEntity.ok().body("Imagem enviada com sucesso");
+			} else {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Id invalido não localizado");
+			}
+		} catch (FileNotFoundException e) {
+			logger.info(">>>>>> api manipula file upload arquivo não encontrado");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Arquivo nao encontrado");
+		} catch (FileUploadException e) {
+			logger.info(">>>>>> api manipula file upload erro no upload");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Falha ao enviar o arquivo");
+		} catch (IOException e) {
+			logger.info(">>>>>> api manipula file upload erro de i/o => " + e.getMessage());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Falha erro de I/O");
+		} catch (NumberFormatException e) {
+			logger.info(">>>>>> api manipula file upload erro de i/o => " + e.getMessage());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Id invalido");
+		}
+	}
+
+}
---
+++ sigvs/src/main/java/com/fatec/sigvs/servico/ProdutoServico.java
@@ -0,0 +1,75 @@
+package com.fatec.sigvs.servico;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import com.fatec.sigvs.model.Catalogo;
+import com.fatec.sigvs.model.IProdutoRepository;
+import com.fatec.sigvs.model.Imagem;
+import com.fatec.sigvs.servico.ImagemServico;
+import com.fatec.sigvs.model.Produto;
+
+@SuppressWarnings("unused")
+@Service
+public class ProdutoServico implements IProdutoServico {
+	Logger logger = LogManager.getLogger(this.getClass());
+	@Autowired
+	IProdutoRepository repositoryP;
+	@Autowired
+	ImagemServico imagemServico;
+
+	@Override
+	public List<Catalogo> consultaCatalogo() {
+		logger.info(">>>>>> produto servico consulta catalogo iniciado");
+		Catalogo c = null;
+		List<Catalogo> lista = new ArrayList<>();
+		List<Produto> listaP = repositoryP.findAll();
+		List<Imagem> listaI = imagemServico.getAll();	
+		for (Produto p : listaP) {
+			for (Imagem i : listaI) {
+				if (p.getId().equals(i.getId())) {
+					c = new Catalogo(p.getId(), p.getDescricao(), p.getCategoria(), p.getCusto(),p.getQuantidadeNoEstoque(), i.getArquivo());
+					lista.add(c);
+				}
+			}
+		}
+		return lista;
+	}
+
+
+	@Override
+	public List<Produto> consultaPorDescricao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Optional<Produto> cadastrar(Produto produto) {
+		logger.info(">>>>>> servico cadastrar produto iniciado ");
+		return Optional.ofNullable(repositoryP.save(produto));
+
+	}
+	@Override
+	public Optional<Produto> consultarPorId(String id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+	@Override
+	public Optional<Produto> atualizar(Long id, Produto produto) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+	@Override
+	public void excluir(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
